0be89e35fc4a917ae12b5a246f111146c8c23330
narenas <nicolas.arenas@gmail.com>
2019-03-22 21:10:36 +0100

    Things should be starting here

--- project/config.py
+++ project/config.py
@@ -1,10 +1,6 @@
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-username = "nico"
-password = "nico"
-dbhost = "postdb"
-database = "Mydatabase"
 SQLALCHEMY_ECHO = False
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = "sqlite:///../app.db"
+SQLALCHEMY_DATABASE_URI = "sqlite:///app.db"
